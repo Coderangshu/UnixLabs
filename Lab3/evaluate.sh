cd82d6f2ce69a947973647dab1b615cb7a7ff3a6
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2025-07-07 10:10:58 +0530

    Lab3 evaluate.sh updated again

--- Lab3/evaluate.sh
+++ Lab3/evaluate.sh
@@ -3,6 +3,9 @@
 cd /home/.evaluationScripts/.bodhiFiles
 [ -f answer.txt ] && rm answer.txt
 cp /home/labDirectory/answer.txt /home/.evaluationScripts/.bodhiFiles/
-rm -rf /home/labDirectory/*
+# Delete everything in /home/labDirectory except answer.txt
+for item in /home/labDirectory/*; do
+    [ "$(basename "$item")" != "answer.txt" ] && rm -rf "$item"
+done
 cp -r /home/.evaluationScripts/.bodhiFiles/studentDirectory/* /home/labDirectory/
 python3 /home/.evaluationScripts/.bodhiFiles/autograder.py
