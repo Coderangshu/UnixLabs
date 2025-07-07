0ede89e1d7c03e20be3b851cb852424a6085da59
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2025-07-07 10:06:20 +0530

    Lab3 evaluate.sh updated

--- Lab3/evaluate.sh
+++ Lab3/evaluate.sh
@@ -2,7 +2,7 @@
 
 cd /home/.evaluationScripts/.bodhiFiles
 [ -f answer.txt ] && rm answer.txt
-mv /home/labDirectory/answer.txt /home/.evaluationScripts/.bodhiFiles/
+cp /home/labDirectory/answer.txt /home/.evaluationScripts/.bodhiFiles/
 rm -rf /home/labDirectory/*
 cp -r /home/.evaluationScripts/.bodhiFiles/studentDirectory/* /home/labDirectory/
 python3 /home/.evaluationScripts/.bodhiFiles/autograder.py
